6c50135b0bec266883075b7e890de9f83fad608a
yasuno0327 <5315076@ed.tus.ac.jp>
2019-12-05 18:04:40 +0900

    Use yahoo fainance

--- get_stock_data.py
+++ get_stock_data.py
@@ -1,28 +1,27 @@
-import datetime
+from datetime import datetime, timedelta
 from lib.data_serializer import DataSerializer
 from pymongo import MongoClient
-import pandas_datareader.data as web
 import numpy as np
+import pandas_datareader.data as web
+import pandas as pd
 
-
-def daterange(_start, _end):
-    for n in range((_end - _start).days):
-        yield _start + datetime.timedelta(n)
-
+def date_range(_start, _end):
+        for i in range((_end-_start).days):
+                yield _start + timedelta(i)
 
 client = MongoClient('0.0.0.0', 27017)
 db = client.trump_stock
-collection = db.dow_jones
+collection = db.djia
 
-year = 2010
-
-start = datetime.datetime(year, 1, 1)
-end = datetime.datetime(year, 12, 31)
-djia = web.DataReader('DJIA', 'fred', start=start, end=start)['DJIA']
-
-for date in daterange(start, end):
-    djia = web.DataReader('DJIA', 'fred', start=date, end=date)['DJIA']
-    if(len(djia) == 0 or np.isnan(djia[0])):
-        collection.insert_one({'date': date, 'price': 0})
-    else:
-        collection.insert_one({'date': date, 'price': djia[0]})
+start = datetime(2017, 1, 20)
+end = datetime.now()
+djia = web.DataReader('dia', 'yahoo', start=start, end=end)
+for pd_date in djia.index:
+        date = pd_date.to_pydatetime()
+        stock_info = djia.loc[pd_date].to_dict()
+        stock_info = {k.lower(): v
+                for k, v in
+                stock_info.items()
+        }
+        stock_info['date'] = date
+        collection.insert_one(stock_info)
