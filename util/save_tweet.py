7599b155296b8d356adaed95f5b2607cbcdc6738
yasuno0327 <5315076@ed.tus.ac.jp>
2019-12-05 18:25:15 +0900

    Save pure data

--- util/save_tweet.py
+++ util/save_tweet.py
@@ -10,13 +10,6 @@ collection = db.tweets
 
 def save_db(year_datas):
     for data in year_datas:
-        tweet_date = data['date']
-
-        if tweet_date.hour < 9 and tweet_date.minute < 30: # 9:30よりも前は前日扱いする
-            data['date'] -= timedelta(days=1)
-        if tweet_date.hour > 15: # 取引所は16:00 closeなので16:00以降は翌日扱いする
-            data['date'] += timedelta(days=1)
-        data['date'] = data['date'].replace(minute=0, hour=0, second=0, microsecond=0)
         collection.insert_one(data)
 
 
