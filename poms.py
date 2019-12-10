2d384dcd35e6b75e80b3f32a8c69285d43228f90
yasuno0327 <5315076@ed.tus.ac.jp>
2019-12-11 08:41:54 +0900

    v2

--- poms.py
+++ poms.py
@@ -1,11 +1,7 @@
 
 from datetime import datetime, timedelta
 import numbers
-from lib.data_serializer import DataSerializer
-from lib.text_evaluator import TextEvaluator
-from lib.stock_data_reader import StockDataReader
-from lib.correlation_evaluator import CorrelationEvaluator
-from pymongo import MongoClient, DESCENDING
+from pymongo import MongoClient
 import matplotlib.pyplot as plt
 import numpy as np
 from emotion_recognition.emotion_predictor import EmotionPredictor
@@ -13,7 +9,13 @@ from statistics import mean
 from statsmodels.tsa.stattools import grangercausalitytests
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
+client = MongoClient('0.0.0.0', 27017)
+db = client.trump_stock
+tweet_col = db.days_tweets
+dow_col = db.djia
+maxlag = 20
 
 def zscore(x, axis=None):
     xmean = np.array(x).mean(axis=axis, keepdims=True)
@@ -28,99 +30,103 @@ def fluctuating(base, changed):
     return base-changed
 
 def granger_causality(df, time_key, causality_key):
-    return grangercausalitytests(df[[time_key, causality_key]], maxlag=20)
+    return grangercausalitytests(df[[time_key, causality_key]], maxlag=maxlag)
 
-# Initialize db
-client = MongoClient('0.0.0.0', 27017)
-db = client.trump_stock
-tweet_col = db.tweets
-dow_col = db.djia
+def get_stock_data(data_type, date):
+    base_date = None
+    comparison_date = None
+    if data_type == 'before_open':
+        base_date = date - timedelta(1)
+        base_type = 'close'
+        comparison_date = date
+        comparison_type = 'open'
+    elif data_type == 'opening':
+        base_date = date
+        base_type = 'open'
+        comparison_date = date
+        comparison_type = 'close'
+    elif data_type == 'after_close':
+        base_date = date
+        base_type = 'close'
+        comparison_date = date + timedelta(1)
+        comparison_type = 'open'
+    else:
+        return None
+    base_stock = dow_col.find_one({'date': base_date})
+    while base_stock is None:
+        base_date -= timedelta(1)
+        base_type = 'close'
+        base_stock = dow_col.find_one({'date': base_date})
+
+    comparison_stock = dow_col.find_one({'date': comparison_date})
+    while comparison_stock is None:
+        comparison_date += timedelta(1)
+        base_type = 'open'
+        comparison_stock = dow_col.find_one({'date': comparison_date})
+
+    return {'base': base_stock[base_type], 'comparison': comparison_stock[comparison_type]}
+
 take_office_at = datetime(2017, 1, 20)
-tweets = tweet_col.find({'date': {'$gte': take_office_at}}).sort('date', 1)
-timeseries = []
+days_tweets = tweet_col.find({'date': {'$gte': take_office_at}}).sort('date', 1)
 rate_datas = []
 fluctuation_datas = []
-
-tweets_of_days = []
-day_first = tweets[0]
+prices = []
+tweet_texts = []
+dates = []
 
 # Initialize poms model
 model = EmotionPredictor(classification='poms', setting='mc')
 
-# days tweet sentiment vs stock price
-anger = []
-depression = []
-fatigue = []
-vigour = []
-tension = []
-confusion = []
+for tweets in days_tweets:
+    texts = ''
+    for tweet in tweets['datas']:
+        texts += tweet['text'] + " "
+    tweet_texts.append(texts)
+    price = get_stock_data(tweets['data_type'], tweets['date'])
+    if price is None:
+        print('continue')
+        continue
+    rate = rate_of_change(price['base'], price['comparison'])
+    fluctuation = fluctuating(price['base'], price['comparison'])
+    rate_datas.append(rate)
+    prices.append(price['comparison'])
+    dates.append(tweets['date'])
+    fluctuation_datas.append(fluctuation)
 
-for tweet in tweets:
-    tweeted_at = tweet['date']
-    opened_at = tweeted_at.replace(hour=9,minute=30,second=0,microsecond=0)
-    closed_at = tweeted_at.replace(hour=16,minute=0,second=0,microsecond=0)
-    tweeted_day = tweeted_at.replace(hour=0, minute=0,second=0,microsecond=0)
-    base_stock = {}
 
-    if tweeted_at < opened_at: # 開場前 前日の情報
-        tweets_of_days.append(tweet['text'])
-    else: # 閉場後 営業時間内 当日の情報
-        if day_first['date'].date() != tweet['date'].date():
-
-            # 株価情報取得 Noneの場合は飛ばす
-            prev_tweet_day = day_first['date'].replace(hour=0,minute=0,second=0,microsecond=0)
-            base_stock = dow_col.find_one({'date': prev_tweet_day})
-            if base_stock is None:
-                tweets_of_days = []
-                day_first = tweet
-                tweets_of_days.append(tweet['text'])
-                continue
-
-            # 感情指数を計算し、保持
-            anger.append(max(model.predict_probabilities(tweets_of_days)['Anger']))
-            depression.append(max(model.predict_probabilities(tweets_of_days)['Depression']))
-            fatigue.append(max(model.predict_probabilities(tweets_of_days)['Fatigue']))
-            vigour.append(max(model.predict_probabilities(tweets_of_days)['Vigour']))
-            tension.append(max(model.predict_probabilities(tweets_of_days)['Tension']))
-            confusion.append(max(model.predict_probabilities(tweets_of_days)['Confusion']))
-
-            # 株価情報を計算し、保持
-            prev_tweet_day = day_first['date'].replace(hour=0,minute=0,second=0,microsecond=0)
-            base_stock = dow_col.find_one({'date': prev_tweet_day})
-            rate_datas.append(rate_of_change(base_stock['open'], base_stock['close'])) # closeまでに株価に反映されていると考える
-            fluctuation_datas.append(fluctuating(base_stock['open'], base_stock['close']))
-
-            tweets_of_days = [] # 次の日なので初期化
-            day_first = tweet
-            tweets_of_days.append(tweet['text']) # ここから再スタート
-        else:
-            tweets_of_days.append(tweet['text'])
-
+result = model.predict_probabilities(tweet_texts)
 df = pd.DataFrame({
-    'rate': rate_datas,
-    'fluctuation': fluctuation_datas,
-    'anger': anger,
-    'depression': depression,
-    'fatigue': fatigue,
-    'tension': tension,
-    'confusion': confusion
+    'rate': zscore(rate_datas),
+    'fluctuation': zscore(fluctuation_datas),
+    'prices': zscore(prices),
+    'anger': zscore(result['Anger'].values),
+    'depression': zscore(result['Depression'].values), # ゆううつ
+    'fatigue': zscore(result['Fatigue'].values),
+    'tension': zscore(result['Tension'].values), # 緊張
+    'confusion': zscore(result['Confusion'].values),
+    'vigour': zscore(result['Vigour'].values),
+    'dates': dates
 })
 
-f_datas = []
-p_datas = []
-mood_index = ['anger', 'depression', 'fatigue', 'tension', 'confusion']
+df.plot(x='dates', xlim=[datetime(2017,1,20),datetime(2017,4,20)], subplots=True, layout=(3,3), figsize=(9,6), title='zscore')
+plt.savefig('result/images/zscore.png')
+plt.close()
+
+mood_index = ['anger', 'depression', 'fatigue', 'tension', 'confusion', 'vigour']
+data_index = ['rate', 'fluctuation', 'prices']
+
+# response = granger_causality(df, 'rate', 'anger')
+# print(response[0])
 
 # グレンジャー因果検定
-for mood in mood_index:
-    response = granger_causality(df, 'rate', mood)
-    ftest = lag['ssr_ftest']
-    f = ftest[0]
-    p = ftest[1]
-    f_datas.append(f)
-    p_datas.append(p)
-    f_df = pd.DataFrame({
-    'f': f_datas,
-    'p': p_datas
-    })
-
-    f_df.to_csv(f'result/{mood}.csv')
+for data in data_index:
+    for mood in mood_index:
+        response = granger_causality(df, data, mood)
+        f_datas = []
+        p_datas = []
+        for key in response:
+            ftest = response[key][0]['ssr_ftest']
+            f_datas.append(ftest[0])
+            p_datas.append(ftest[1])
+        g_df = pd.DataFrame({'F': f_datas, 'p': p_datas}, index=[i for i in range(1, maxlag+1)])
+        g_df.to_csv(f'result/{data}/{mood}.csv', index_label='Lag')
